1b4c41c8480b3127e8f6f807e7a151bf839014aa
Mohammad Ali <maghorbani759@gmail.com>
2023-01-05 12:42:34 +0330

    [ADD] working simple http response, hello world wrapped in an H1 tag

--- src/httpServer.cpp
+++ src/httpServer.cpp
@@ -1,6 +1,7 @@
 #include "httpServer.h"
 
 #include <chrono>
+#include <exception>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
@@ -9,29 +10,39 @@
 #include <sys/types.h>
 #include <thread>
 #include <unistd.h>
+
 namespace http {
 
 void Server::listen(uint16_t port) {
 
+  bzero((char *)&m_address, sizeof(m_address));
+  m_address.sin_family = AF_INET;
+  m_address.sin_port = htons(port);
   m_address.sin_addr.s_addr = INADDR_ANY;
-  m_address.sin_port = port;
-  memset(m_address.sin_zero, '\0', sizeof(m_address.sin_zero));
   m_socket = socket(AF_INET, SOCK_STREAM, 0);
-  std::cout << "socket: " << m_socket << std::endl;
+  if (m_socket < 0) {
+    throw "error creating socket";
+  }
+  int res = ::bind(m_socket, reinterpret_cast<sockaddr *>(&m_address),
+                   static_cast<socklen_t>(sizeof(m_address)));
+
+  if (res != 0) {
+    throw "error binding";
+  }
+  res = ::listen(m_socket, 10000);
+  if (res != 0) {
+    throw "error listening";
+  }
   int new_socket;
-  size_t addr_len = sizeof(m_address);
-  std::cout << bind(m_socket, (sockaddr *)(&m_address), sizeof(m_address))
-            << std::endl;
-
-  ::listen(m_socket, 10);
-
+  std::string html = "<h1>Hello World</h1>";
+  std::string resp = "HTTP/1.1 200 OK\nContent-Type: "
+                     "text/html;charset=UTF-8\nContent-Length:" +
+                     std::to_string(html.length()) + "\n\n" + html;
   while (true) {
-    new_socket = accept(m_socket, reinterpret_cast<sockaddr *>(&m_address),
-                        (socklen_t *)(&addr_len));
+    new_socket = accept(m_socket, nullptr, nullptr);
     char buff[1000];
     read(new_socket, buff, 1000);
-    printf("%s\n", buff);
-    write(new_socket, "Hello", strlen("Hello"));
+    write(new_socket, resp.c_str(), resp.length());
     close(new_socket);
   }
 }
